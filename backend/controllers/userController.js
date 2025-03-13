e4eb3ff75f515b84bd5ff9dde10f2d8816e5a966
vivian5285 <wangjiali240@gmail.com>
2025-03-14 05:29:50 +0800

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "获取用户失败", error });
+  }
+};
+
+const createUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: "创建用户失败", error });
+  }
+};
+
+module.exports = { getUsers, createUser };
