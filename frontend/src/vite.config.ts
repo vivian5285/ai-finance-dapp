87d0a46cf833f7445bc546b527d50c5210923508
vivian5285 <wangjiali240@gmail.com>
2025-03-15 21:39:41 +0800

    Update vite.config.ts

--- frontend/src/vite.config.ts
+++ frontend/src/vite.config.ts
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174, // 你可以改成其他端口，比如 5174
+    port: 5173, // 你可以改成 5173
     strictPort: true,
+    host: true, // 允许外部访问
+  },
+  define: {
+    global: "window", // 解决 "Cannot redefine property: ethereum" 问题
   },
 });
