d9926ff7b698fd5225d7c99a617bd97ee19e800c
vivian5285 <wangjiali240@gmail.com>
2025-03-15 18:37:13 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,16 @@ import Home from "./pages/Home";
 import Invest from "./pages/Invest";
 import Market from "./pages/Market";
 import Profile from "./pages/Profile";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/invest" element={<Invest />} />
         <Route path="/market" element={<Market />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-      <Footer />
     </Router>
   );
 };
