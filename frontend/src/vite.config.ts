a97ec0fa08e8373a6941bd121959cadee436e676
vivian5285 <wangjiali240@gmail.com>
2025-03-15 21:07:20 +0800

    Update vite.config.ts

--- frontend/src/vite.config.ts
+++ frontend/src/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // 你可以改成其他端口，比如 5174
+    port: 5174, // 你可以改成其他端口，比如 5174
     strictPort: true,
   },
 });
